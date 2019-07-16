42d890f93c9d5ec8c8308d6fc9a950d549227cdf
Nejc Hirci <nhirci@gmail.com>
2019-07-16 13:19:36 +0200

    Working version of sample laboratory page with missing proper fonts.

---
+++ content/personal/_index.md
@@ -0,0 +1,8 @@
+---
+title: "Personal"
+date: 2019-07-16T11:33:26+02:00
+draft: false
+url: "/employees"
+bannerText: ""
+bannerImg: ""
+---
---
+++ runHugo.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Build the project.
+hugo server --ignoreCache --disableFastRender
+# if using a theme, replace with `hugo -t <YOURTHEME>`
