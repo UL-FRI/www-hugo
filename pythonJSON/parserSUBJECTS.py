e88a3044ce26b03898116825e890dbee90663726
Toxic <nhirci@gmail.com>
2019-10-06 10:40:25 +0200

    Parser for subjects to add english titles missing from last version.

---
+++ pythonJSON/parserSUBJECTS.py
@@ -0,0 +1,19 @@
+import json
+import re
+import io
+import os
+import requests
+
+with io.open('./sampleJSON/subjects.json', 'r') as srcOne, io.open('./sampleJSON/persons.json', 'r') as srcTwo:
+    loadSrc = json.load(srcOne)
+    loadPersons = json.load(srcTwo)
+
+    for x in loadSrc:
+        for y in loadPersons['persons']:
+            if y['subjects']:
+                for sub in y['subjects']:
+                    if x['course_code'] == sub['course_code']:
+                        x['title'] = sub['title']
+
+    with io.open('./sampleJSON/predmeti.json', 'w+') as dst:
+        json.dump(loadSrc, dst)
